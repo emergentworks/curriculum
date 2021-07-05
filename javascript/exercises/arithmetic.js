959badfd41953bc1eee6021c6adee62ec3207598
Hailey Miller <17454012+mermatriarchy@users.noreply.github.com>
2021-07-04 21:55:41 -0400

    add arithmetic js practice exercises

---
+++ javascript/exercises/arithmetic.js
@@ -0,0 +1,75 @@
+// PART ONE: Literals
+// write a line of code that adds 2,930 and 6,272 together,
+// assign it to a constant, and print it to the console
+
+// write a line of code that multiplies 987 and 3,129,
+// assign it to a constant, and print it to the console
+
+// write a line of code that subtracts 452 from 982,
+// assign it to a constant, and print it to the console
+
+// write a line of code that divides 1560 by 20,
+// assign it to a const, and print it to the console
+
+// write a line of code that calculates 8 to the third power,
+// assign it to a const, and print it to the console
+
+// write a line of code that finds the remainder of 86 divided by 3,
+// assign it to a const, and print it to the console
+
+// write a line of code that increments 100,
+// assign it to a const, and print it to the console
+
+// write a line of code that decrements 100, 
+// assign it to a const, and print it to the console
+
+// write a line of code that adds 8,237 and 836, then subtracts 304.
+// assign it to a const, and print it to the console
+
+// write a line of code that multiplies 986 by 4, then divide by 2.
+// assign it to a const, and print it to the console
+
+// write a line of code that subtracts 954 from 1254, then multiply
+// it by 7. assign it to a const, and print it to the console
+
+// write a line of code that adds 633 and 877, then divide by 5.
+// assign it to a const, and print it to the console
+
+// PART TWO: Variables and Expressions
+// write an expression with two variables, x and y, that adds two numbers together.
+// if x is 238 and y is 9,271, what does the expression evaluate to?
+// print the answer to the console
+
+// write an expression with three variables, a, b and c, that adds a and b together,
+// then divides the answer by c. 
+// if a is 16, b is 14, and c is 6, what does the expression evaluate to?
+// print the answer to the console
+
+// write an expression with three variables, x, y and z, that subtracts x from y,
+// then multiplies the result by z.
+// if x is 87, z is 27 and z is 4, what does the expression evaluate to?
+// bonus: if you decrement the total, what does the expression evaluate to?
+// print the answer to the console
+
+// write an expression with four variables, a, b, c and d, that adds together a and b,
+// then adds together c and d, and subtracts the first result from the second.
+// if a is 132, b is 258, c is 89 and d is 41, what does the expression evaluate to?
+// bonus: if you increment the total, what does the expression evaluate to?
+// print the answer to the console
+
+// PART THREE: Word problems
+// Hannah is going to a coding bootcamp that last 16 weeks where she'll be in
+// class 5 days per week for 8 hours. How many hours total will Hannah be in class?
+
+// Joel is working at his neighborhood's community garden. The garden is divided 
+// into 4 large sections and has 36 total rows of plants. If each section has the
+// same number of rows, how many rows of plants does each section have?
+
+// Charlene is handing out free samples at the grocery store. She starts with 48
+// samples and gives away 14, then a coworker brings 22 more. How many samples
+// does Charlene have left?
+
+// Tara, James and Terrence are working at a coffee shop and will split the tips
+// at the end of the day. In their first shift they make $106 in tips, during the 
+// second shift they make $42, during the third shift they make $89 in tips. How
+// much tip money will each person take home?
